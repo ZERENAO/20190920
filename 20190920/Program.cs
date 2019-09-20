0659fc5dffad90c5177e59c8c6dadb1a7a22e8f4
Becsaj Ádám <becsaj.adam@vereb.dc>
2019-09-20 16:39:12 +0200

    Add project files.

---
+++ 20190920/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190920
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia, hogy hívnak?");
+            var n = Console.ReadLine();
+            Console.WriteLine($"Ó, milyen szép név az hogy {n}!");
+            Console.WriteLine($"Szerecc pogramozni {n}? :3");
+            var v = Console.ReadLine();
+            if(v.ToLower() == "igen")
+            {
+                Console.WriteLine("Szuper, akkor örökké barátok leszünk C:");
+            }
+            else
+            {
+                Console.WriteLine("Akkor kabbe!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
